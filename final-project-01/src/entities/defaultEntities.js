b522ba35a60bfce6c4dd21746d77d446271de84c
Pedro Bruning <pedrohboschier@gmail.com>
2022-11-22 18:16:31 -0300

    chore: add remote changes

--- final-project-01/src/entities/defaultEntities.js
+++
@@ -1,9 +0,0 @@
-const House = require('./house')
-const Neighborhood = require('./neighborhood')
-const Customer = require('./customer')
-
-module.exports = {
-    House,
-    Neighborhood,
-    Customer
-}
